34fe1e8f441774d9df9f7cf69add8d91cb6124a8
meht12 <100204328+meht12@users.noreply.github.com>
2024-11-09 00:17:28 +0530

    Update api.js

--- Admin-panel-frontend/src/Api/api.js
+++ Admin-panel-frontend/src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://13.201.91.8000";
+const API_BASE_URL = "http://13.201.91.206:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
