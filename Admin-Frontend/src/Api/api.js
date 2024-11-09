d8430226bcad3eec81fabf6b93a99ce45573af9d
Saksham Kamboj <saksham@ekvayu.com>
2024-11-09 16:18:19 +0530

    update final for apis

--- Admin-Frontend/src/Api/api.js
+++ Admin-Frontend/src/Api/api.js
@@ -1,14 +1,12 @@
-// const API_BASE_URL = (() => {
-//   if (import.meta.env.MODE === "development") {
-//     return "http://35.154.97.4:8002";
-//   } else if (import.meta.env.MODE === "production") {
-//     return "/api/proxy";
-//   } else {
-//     return "http://localhost:8000";
-//   }
-// })();
-
-const API_BASE_URL = "/api/proxy";
+const API_BASE_URL = (() => {
+  if (import.meta.env.MODE === "development") {
+    return "http://35.154.97.4:8002";
+  } else if (import.meta.env.MODE === "production") {
+    return "http://35.154.97.4:8002";
+  } else {
+    return "http://localhost:8000";
+  }
+})();
 
 export const refreshToken = async () => {
   const token = sessionStorage.getItem("token");
