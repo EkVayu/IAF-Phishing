a77bc3f296f0d28f5f3419f6f07ea90166f64486
Mehtab <mdmahtabraja2003@gmail.com>
2024-11-30 11:16:26 +0530

    Changes in Agent File

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -47,5 +47,5 @@ urlpatterns = [
     path('logoutall/',knox_views.LogoutAllView.as_view(), name='knox_logoutall'), 
     # path('api/password_reset/',include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
